2e0f6c751c79ec1cf39390d70f918f34eea5b8dd
Javier Estrada <javier.estrada@autentia.com>
2021-01-08 14:31:00 +0100

    Output fixing

--- aws/storage/s3-bucket/outputs.tf
+++ aws/storage/s3-bucket/outputs.tf
@@ -1,4 +1,4 @@
-output "s3_bucket_arn {
+output "s3_bucket_arn" {
     description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
     value = module.this.output.this_s3_bucket_arn
 }
